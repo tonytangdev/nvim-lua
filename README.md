86d78128de946b56a1273f8b41de51b67404c821
tonytangdev <tony.tang.developer@gmail.com>
2022-04-19 21:44:10 +0200

    Setup LSP

--- README.md
+++ README.md
@@ -10,3 +10,5 @@
 
 - [Packer](https://github.com/wbthomason/packer.nvim#quickstart) (Package manager)
 - [Nightfly](https://github.com/bluz71/vim-nightfly-guicolors#installation) (Theme)
+- [LSP + null-ls + lsp-ts-utils](https://jose-elias-alvarez.medium.com/configuring-neovims-lsp-client-for-typescript-development-5789d58ea9c) (Configure js/ts for code helps)
+- [plenary](https://github.com/nvim-lua/plenary.nvim) (Necessary for LSP)
--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 require('theme')
+require('lsp')
 
 vim.cmd([[
   augroup packer_user_config
@@ -9,4 +10,10 @@ vim.cmd([[
 return require('packer').startup(function()
 	use 'wbthomason/packer.nvim' -- Packer can manage itself
 	use 'bluz71/vim-nightfly-guicolors' -- Theme
+	
+	-- LSP for code completion, snippets, etc
+	use "neovim/nvim-lspconfig"
+	use "jose-elias-alvarez/null-ls.nvim"
+	use "jose-elias-alvarez/nvim-lsp-ts-utils"
+	use "nvim-lua/plenary.nvim"
 end)
