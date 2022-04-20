386cd17036474bc81444c4b500b7593c3cb66b24
tonytang-planity <tony.tang@planity.com>
2022-04-20 18:10:34 +0200

    display lines number

--- init.lua
+++ init.lua
@@ -1,18 +1,21 @@
 vim.g.mapleader = " "
+require('general')
 require('theme')
 require('lsp')
 require('code-completion')
 require('tree-sitter')
 require('telescope')
 require('tree')
+require('hop')
 
 vim.cmd([[
+  set number
+  set cursorline
   augroup packer_user_config
     autocmd!
     autocmd BufWritePost plugins.lua source <afile> | PackerCompile
   augroup end
 ]])
-
 
 return require('packer').startup(function()
 	use 'wbthomason/packer.nvim' -- Packer can manage itself
