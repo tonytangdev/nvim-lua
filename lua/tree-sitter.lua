08f09f5ae2b57d51d32a242b6cb603b0615f1100
tonytang-planity <tony.tang@planity.com>
2022-04-20 10:29:19 +0200

    Add html to tree-sitter

--- lua/tree-sitter.lua
+++ lua/tree-sitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
 
-	ensure_installed = { "javascript", "typescript", "jsdoc", "css", "dot", "dockerfile", "python", "json", "jsonc", "lua", "tsx", "yaml" },
+	ensure_installed = { "javascript", "typescript", "jsdoc", "css", "dot", "dockerfile", "python", "json", "jsonc", "lua", "tsx", "yaml", "bash", "html" },
 
 	sync_install = false,
 
